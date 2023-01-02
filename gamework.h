4452696591a849dfa80695467060ed55992fa897
shreyaspranav <shrepranav5@gmail.com>
2023-01-02 20:11:00 +0530

    Converted a c project to a c++ project

--- gamework.h
+++ gamework.h
@@ -1,12 +1,12 @@
+#include <iostream>
 #include <GLFW/glfw3.h>
-#include <stdio.h>
-#include <stdbool.h>
 
 void print_glfw_init()
 {
-    printf("%d", glfwInit());
+    std::cout << glfwInit() << std::endl;
 }
 
+// Window stuff
 struct WindowSpecs {
     unsigned int window_width, window_height;
     const char* window_title;
@@ -15,3 +15,17 @@ struct WindowSpecs {
     unsigned int window_min_width, window_max_width, window_min_height, window_max_height;
     bool window_vsync;
 };
+
+void gameLoop();
+double getGameUpTime();
+
+
+#ifdef USING_GAMEWORK
+
+void gameLoop(double timestep) {
+    glfwPollEvents();
+    glfwSwapBuffers();
+}
+
+
+#endif
